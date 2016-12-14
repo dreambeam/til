a8d34cbad1391d0a3a7b0221fc855c21e322a392
Bleza <dreambeam77@gmail.com>
2016-12-14 09:09:17 +0900

    system directory

---
+++ system/Logstash/start.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+# /usr/local/bin/start.sh
+# Start Logstash service
+#
+# spujadas 2015-10-09; added initial pidfile removal and graceful termination
+
+# WARNING - This script assumes that the ELK services are not running, and is
+#   only expected to be run once, when the container is started.
+#   Do not attempt to run this script if the ELK services are running (or be
+#   prepared to reap zombie processes).
+
+
+## handle termination gracefully
+
+_term() {
+  echo "Terminating Logstash"
+  service logstash stop
+  exit 0
+}
+
+trap _term SIGTERM
+
+
+## remove pidfiles in case previous graceful termination failed
+# NOTE - This is the reason for the WARNING at the top - it's a bit hackish,
+#   but if it's good enough for Fedora (https://goo.gl/88eyXJ), it's good
+#   enough for me :)
+
+rm -f /var/run/elasticsearch/elasticsearch.pid /var/run/logstash.pid \
+  /var/run/kibana5.pid
+
+## initialise list of log files to stream in console (initially empty)
+OUTPUT_LOGFILES=""
+
+
+## start services as needed
+
+### crond
+
+service cron start
+
+
+
+
+### Logstash
+
+if [ -z "$LOGSTASH_START" ]; then
+  LOGSTASH_START=1
+fi
+if [ "$LOGSTASH_START" -ne "1" ]; then
+  echo "LOGSTASH_START is set to something different from 1, not starting..."
+else
+  # override LS_HEAP_SIZE variable if set
+  if [ ! -z "$LS_HEAP_SIZE" ]; then
+    awk -v LINE="LS_HEAP_SIZE=\"$LS_HEAP_SIZE\"" '{ sub(/^LS_HEAP_SIZE=.*/, LINE); print; }' /etc/init.d/logstash \
+        > /etc/init.d/logstash.new && mv /etc/init.d/logstash.new /etc/init.d/logstash && chmod +x /etc/init.d/logstash
+  fi
+
+  # override LS_OPTS variable if set
+  if [ ! -z "$LS_OPTS" ]; then
+    awk -v LINE="LS_OPTS=\"$LS_OPTS\"" '{ sub(/^LS_OPTS=.*/, LINE); print; }' /etc/init.d/logstash \
+        > /etc/init.d/logstash.new && mv /etc/init.d/logstash.new /etc/init.d/logstash && chmod +x /etc/init.d/logstash
+  fi
+
+  service logstash start
+  OUTPUT_LOGFILES+="/var/log/logstash/logstash-plain.log "
+fi
+
+
+tail -f $OUTPUT_LOGFILES &
+wait
